61a5823d1296436d8e8e86f80eb43ec8f4c3f923
Justin de Ruiter <justinderuiter@ziggo.nl>
2024-01-11 16:52:30 +0100

    Fix lzo_uint being incorrectly represented as c_ulonglong instead of usize

--- src/lib.rs
+++ src/lib.rs
@@ -20,9 +20,9 @@ macro_rules! lzo_func_decl {
     ($name:ident) => {
         pub fn $name(
             src: *const core::ffi::c_uchar,
-            src_len: core::ffi::c_ulonglong,
+            src_len: usize,
             dst: *mut core::ffi::c_uchar,
-            dst_len: *mut core::ffi::c_ulonglong,
+            dst_len: *mut usize,
             wrkmem: *mut core::ffi::c_void,
         ) -> core::ffi::c_int;
     };
@@ -34,9 +34,9 @@ macro_rules! lzo_compress_level_func_decl {
     ($name:ident) => {
         pub fn $name(
             src: *const core::ffi::c_uchar,
-            src_len: core::ffi::c_ulonglong,
+            src_len: usize,
             dst: *mut core::ffi::c_uchar,
-            dst_len: *mut core::ffi::c_ulonglong,
+            dst_len: *mut usize,
             wrkmem: *mut core::ffi::c_void,
             compression_level: core::ffi::c_int,
         ) -> core::ffi::c_int;
@@ -49,34 +49,34 @@ macro_rules! lzo_999_func_decls {
     ($unsafe_name:ident, $level_name:ident, $safe_name:ident) => {
         pub fn $unsafe_name(
             src: *const core::ffi::c_uchar,
-            src_len: core::ffi::c_ulonglong,
+            src_len: usize,
             dst: *mut core::ffi::c_uchar,
-            dst_len: *mut core::ffi::c_ulonglong,
+            dst_len: *mut usize,
             wrkmem: *mut core::ffi::c_void,
             dict: *const core::ffi::c_uchar,
-            dict_len: core::ffi::c_ulonglong,
+            dict_len: usize,
         ) -> core::ffi::c_int;
 
         pub fn $level_name(
             src: *const core::ffi::c_uchar,
-            src_len: core::ffi::c_ulonglong,
+            src_len: usize,
             dst: *mut core::ffi::c_uchar,
-            dst_len: *mut core::ffi::c_ulonglong,
+            dst_len: *mut usize,
             wrkmem: *mut core::ffi::c_void,
             dict: *const core::ffi::c_uchar,
-            dict_len: core::ffi::c_ulonglong,
+            dict_len: usize,
             cb: *mut crate::lzoconf::lzo_callback_t,
             compression_level: core::ffi::c_int,
         ) -> core::ffi::c_int;
 
         pub fn $safe_name(
             src: *const core::ffi::c_uchar,
-            src_len: core::ffi::c_ulonglong,
+            src_len: usize,
             dst: *mut core::ffi::c_uchar,
-            dst_len: *mut core::ffi::c_ulonglong,
+            dst_len: *mut usize,
             wrkmem: *mut core::ffi::c_void,
             dict: *const core::ffi::c_uchar,
-            dict_len: core::ffi::c_ulonglong,
+            dict_len: usize,
         ) -> core::ffi::c_int;
     };
 }
