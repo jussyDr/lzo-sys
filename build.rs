2001765d07ad1cc8877c4f46fff8bfc13e9c1a6c
Sergey Mikhailov <sergey.michailov@gmail.com>
2025-04-11 15:01:27 +0300

    enable building with CMake 4.0

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
 fn main() {
-    let mut install_dir = cmake::build("lzo");
+    let mut install_dir = cmake::Config::new("lzo")
+        .define("CMAKE_POLICY_VERSION_MINIMUM", "3.5")
+        .build();
+
     install_dir.push("lib");
 
     println!("cargo:rustc-link-search=native={}", install_dir.display());
